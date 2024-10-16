ab566fc362ad0868175e35177918b01981f6d4af
ripplecrest <2004shivang@gmail.com>
2024-10-16 17:36:14 +0530

    Added function to give results

--- MNIST dataset/MNIST_dataset_model.py
+++ MNIST dataset/MNIST_dataset_model.py
@@ -91,3 +91,25 @@ def gradient_descent(X, Y, iterations, alpha):
     return W1, b1, W2, b2
 
 W1, b1, W2, b2 = gradient_descent(X_train, Y_train, 500, 0.1)
+
+def make_predictions(X, W1, b1, W2, b2):
+    _, _, _, A2 = forward_prop(W1, b1, W2, b2, X)
+    predictions = get_predictions(A2)
+    return predictions
+
+def test_prediction(index, W1, b1, W2, b2):
+    current_image = X_train[:, index, None]
+    prediction = make_predictions(X_train[:, index, None], W1, b1, W2, b2)
+    label = Y_train[index]
+    print("Prediction: ", prediction)
+    print("Label: ", label)
+    
+    current_image = current_image.reshape((28, 28)) * 255
+    plt.gray()
+    plt.imshow(current_image, interpolation='nearest')
+    plt.show()
+    
+test_prediction(0, W1, b1, W2, b2)
+test_prediction(1, W1, b1, W2, b2)
+test_prediction(2, W1, b1, W2, b2)
+test_prediction(3, W1, b1, W2, b2)
